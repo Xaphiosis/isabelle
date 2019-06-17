bd76a2cebe95f6a9bf2e03e5ffcdc17c3d7c388a
Rafal Kolanski <rafal.kolanski@nicta.com.au>
2019-06-17 19:14:52 +1000

    disable using Isabelle fonts for GUI

--- src/Tools/jEdit/src/plugin.scala
+++ src/Tools/jEdit/src/plugin.scala
@@ -401,7 +401,7 @@ class Plugin extends EBPlugin
             if (buffer != null && text_area != null) init_view(buffer, text_area)
           }
 
-          GUI.use_isabelle_fonts()
+          // RAF GUI.use_isabelle_fonts()
 
           spell_checker.update(options.value)
           session.update_options(options.value)
