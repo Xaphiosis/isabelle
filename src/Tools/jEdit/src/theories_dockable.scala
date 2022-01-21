47b2a6997a4a74ac7552fb7a589f632f86551078
Rafal Kolanski <rafal.kolanski@proofcraft.systems>
2022-01-21 14:24:40 +1100

    thinner completed theories outline, tweak ok_color
    The 2021-1 version is too thick. Also make default ok_color more
    cheerful.
    
    I initially implemented a version of this called processed_color, and it
    was re-done and included as ok_color. I still want my setting to
    override it when I switch between other Isabelle versions that might not
    have this, or might have my processed_color.

--- src/Tools/jEdit/src/theories_dockable.scala
+++ src/Tools/jEdit/src/theories_dockable.scala
@@ -194,8 +194,8 @@ class Theories_Dockable(view: View, position: String) extends Dockable(view, pos
             PIDE.options.color_value("failed_color")
           case _ => label.foreground
         }
-      val thickness1 = if (st == Document_Status.Overall_Node_Status.pending) 1 else 3
-      val thickness2 = 4 - thickness1
+      val thickness1 = if (st == Document_Status.Overall_Node_Status.pending) 1 else 2
+      val thickness2 = 3 - thickness1
 
       label.border =
         BorderFactory.createCompoundBorder(
