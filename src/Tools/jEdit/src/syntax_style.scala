acd026410661f576dd9904a5a7fcd972015b5d42
Rafal Kolanski <rafal.kolanski@proofcraft.systems>
2022-05-10 22:03:30 +1000

    Remove support for user fonts.
    These mess with font substitution by defaulting to IsabelleText, which
    can support most symbols, and not the actual font we chose for the
    editor window.
    
    Font specification in etc/symbols is now ignored.

--- src/Tools/jEdit/src/syntax_style.scala
+++ src/Tools/jEdit/src/syntax_style.scala
@@ -76,10 +76,6 @@ object Syntax_Style {
   }
 
   object Main_Extender extends SyntaxUtilities.StyleExtender {
-    val max_user_fonts = 2
-    if (Symbol.symbols.font_names.length > max_user_fonts)
-      error("Too many user symbol fonts (" + max_user_fonts + " permitted): " +
-        Symbol.symbols.font_names.mkString(", "))
 
     override def extendStyles(styles: Array[SyntaxStyle]): Array[SyntaxStyle] = {
       val style0 = styles(0)
@@ -92,15 +88,12 @@ object Syntax_Style {
         new_styles(subscript(i.toByte)) = script_style(style, -1)
         new_styles(superscript(i.toByte)) = script_style(style, 1)
         new_styles(bold(i.toByte)) = bold_style(style)
-        for (idx <- 0 until max_user_fonts)
-          new_styles(user_font(idx, i.toByte)) = style
-        for ((family, idx) <- Symbol.symbols.font_index)
-          new_styles(user_font(idx, i.toByte)) = font_style(style, GUI.imitate_font(_, family))
       }
       new_styles(hidden) =
         new SyntaxStyle(hidden_color, null,
-          GUI.transform_font(new Font(font0.getFamily, 0, 1),
-            AffineTransform.getScaleInstance(2.0, font0.getSize.toDouble)))
+          // FIXME RAF: 1.0 is 2.0 in 2019 version
+          GUI.transform_font(new Font(font0.getFamily, 0, font0.getSize),
+            AffineTransform.getScaleInstance(1.0/font0.getSize.toDouble, 1.0)))
       new_styles(control) =
         new SyntaxStyle(style0.getForegroundColor, style0.getBackgroundColor,
           {
@@ -145,11 +138,6 @@ object Syntax_Style {
         mark(offset, a, _ => hidden)
         mark(a, b, _ => control)
         mark(b, end_offset, _ => hidden)
-      }
-
-      Symbol.symbols.lookup_font(sym) match {
-        case Some(idx) => mark(offset, end_offset, user_font(idx, _))
-        case _ =>
       }
 
       offset = end_offset
